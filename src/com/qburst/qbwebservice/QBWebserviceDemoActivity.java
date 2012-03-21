49cfe05be58ec46d5a8e0a53e3d1fff5a7e998e2
Habib Ashraf <habibmashraf@gmail.com>
2012-03-21 12:46:29 +0530

    Correction in list adapter implementation

--- src/com/qburst/qbwebservice/QBWebserviceDemoActivity.java
+++ src/com/qburst/qbwebservice/QBWebserviceDemoActivity.java
@@ -1,3 +1,4 @@
+
 package com.qburst.qbwebservice;
 
 import java.util.ArrayList;
@@ -18,80 +19,89 @@ import com.qburst.qbwebservice.models.QBWebserviceModel;
 import com.qburst.qbwebservice.utilities.QBUtility;
 
 public class QBWebserviceDemoActivity extends Activity implements QBWebserviceListener {
-    
-	ListView _subcategoriesLv;
-	String _email, _password;
-	int _categoryId = 5;
-	QBTestApi _subcategoriesApi;
-	QBInternAdapter _InternAdapter;
-	private Context _context;
-	
-	/** Called when the activity is first created. */
+
+    ListView _subcategoriesLv;
+    String _email, _password;
+    int _categoryId = 5;
+    QBTestApi _subcategoriesApi;
+    QBInternAdapter _InternAdapter;
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         _email = "a";
         _password = "a";
-		_context = this;
-		
-        _subcategoriesLv = (ListView)findViewById(R.id.listView1);
-        
-        _subcategoriesApi = new QBTestApi( _email, _password, _categoryId, QBWebserviceDemoActivity.this);
+
+        _subcategoriesLv = (ListView) findViewById(R.id.listView1);
+
+        _subcategoriesApi = new QBTestApi(_email, _password, _categoryId, QBWebserviceDemoActivity.this);
         _subcategoriesApi.invoke();
-    
+
     }
 
+    @Override
+    public void onSuccess(ArrayList<QBWebserviceModel> subcatList, int apiID) {
 
-	@Override
-	public void onSuccess(ArrayList<QBWebserviceModel> subcatList, int apiID) {
-		
-		_InternAdapter = new QBInternAdapter(subcatList);
-		_subcategoriesLv.setAdapter(_InternAdapter);
-		//_InternAdapter.notifyDataSetChanged();
-	}
+        _InternAdapter = new QBInternAdapter(this, subcatList);
+        _subcategoriesLv.setAdapter(_InternAdapter);
+        // _InternAdapter.notifyDataSetChanged();
+    }
 
+    @Override
+    public void onFail(String errorResponse, int apiID) {
+        QBUtility.showDialogOkWithGoBack("Fetch fail", errorResponse, this);
+    }
 
-	@Override
-	public void onFail(String errorResponse, int apiID) {
-		QBUtility.showDialogOkWithGoBack("Fetch fail", errorResponse, this);
-	}
-	
-	public class QBInternAdapter extends BaseAdapter{
-		
-		ArrayList<QBWebserviceModel> _subcatList;
+    public class QBInternAdapter extends BaseAdapter {
+        LayoutInflater _inflater;
+        ArrayList<QBWebserviceModel> _subcatList;
 
-		public QBInternAdapter(ArrayList<QBWebserviceModel> subcatList) {
-			this._subcatList = subcatList;
-		}
+        public QBInternAdapter(Context context, ArrayList<QBWebserviceModel> subcatList) {
+            this._inflater = LayoutInflater.from(context);
+            this._subcatList = subcatList;
+        }
 
-		@Override
-		public int getCount() {
-			return _subcatList.size();
-		}
+        @Override
+        public int getCount() {
+            return _subcatList.size();
+        }
 
-		@Override
-		public Object getItem(int position) {
-			return _subcatList.get(position);
-		}
+        @Override
+        public Object getItem(int position) {
+            return _subcatList.get(position);
+        }
 
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			
-			convertView = LayoutInflater.from(_context).inflate(R.layout.cell, null);
-			
-			TextView _subcatTv = (TextView)convertView.findViewById(R.id.textView1);
-			_subcatTv.setText(_subcatList.get(position).getName());
-			return convertView;
-		}
-		
-	}
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = _inflater.inflate(R.layout.cell, null);
+
+                holder = new ViewHolder();
+                holder.subCatTextView = (TextView) convertView.findViewById(R.id.textView1);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            // Bind the data efficiently with the holder.
+            holder.subCatTextView.setText(_subcatList.get(position).getName());
+
+            return convertView;
+        }
+    }
+    
+    static class ViewHolder {
+        TextView subCatTextView;
+    }
 }
-
-
